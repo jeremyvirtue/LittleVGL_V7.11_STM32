78964bd21916fb58dbff2052e4898fc20b2d3046
jeremylin777 <1037063188@qq.com>
2021-04-12 18:56:05 +0800

    6.0移植完成

--- USER/main.c
+++ USER/main.c
@@ -6,9 +6,16 @@
 #include "key.h"  
 #include "touch.h" 
 #include "timer.h"
+#include "sram.h"
+#include "lvgl.h"
+#include "lv_port_disp.h"
+#include "lv_port_indev.h"
+#include "lv_apps\demo\demo.h" 
+#include "lv_tests\lv_test_theme\lv_test_theme_1.h" 
+#include "lv_tests\lv_test_theme\lv_test_theme_2.h"
  
  
- 
+ #define TEST_NUM	1
 	
 int main(void)
 { 
@@ -21,8 +28,24 @@ int main(void)
  	LCD_Init();					//LCD初始化 
 	KEY_Init(); 				//按键初始化  
 	TIM3_Int_Init(999,83);	//定时器初始化(1ms中断),用于给lvgl提供1ms的心跳节拍
+	
+	FSMC_SRAM_Init();		//初始化外部sram
 	tp_dev.init();				//触摸屏初始化
+	
+		lv_init();						//lvgl系统初始化
+	lv_port_disp_init();	//lvgl显示接口初始化,放在lv_init()的后面
+	lv_port_indev_init();	//lvgl输入接口初始化,放在lv_init()的后面
+	
+	//通过TEST_NUM的值来选择运行不同的例程
+	#if(TEST_NUM==1)
+		demo_create();			
+	#elif(TEST_NUM==2)
+		lv_test_theme_1(lv_theme_night_init(210, NULL));
+	#else
+		lv_test_theme_2();
+	#endif
 	while(1){
-		
+		tp_dev.scan(0);
+		lv_task_handler();
 	}
 }
